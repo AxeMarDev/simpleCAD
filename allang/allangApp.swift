16f0c8ce47cda82599f8dc55e8a145d15d359bb3
Axell Martinez Avila <niz6@txstate.edu>
2023-04-18 15:10:09 -0500

    Initial Commit

---
+++ allang/allangApp.swift
@@ -0,0 +1,17 @@
+//
+//  allangApp.swift
+//  allang
+//
+//  Created by Axell Martinez on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct allangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
